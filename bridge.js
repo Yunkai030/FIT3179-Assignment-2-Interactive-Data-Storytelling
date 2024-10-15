cf1c83db6a344735354e416c2de44fe702579f79
yunkai huang <81064518+Yunkai030@users.noreply.github.com>
2024-10-15 21:59:55 +1100

    Add files via upload

---
+++ bridge.js
@@ -0,0 +1,26 @@
+var vg_1 = "dot_map"; 
+var vg_2 = "line_chart"
+var vg_3 = "min_vs_max"
+var vg_4 = "pie_chart"
+var vg_5 = "rain_vs_temp"
+
+
+vegaEmbed("#dot_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+vegaEmbed("#line_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+vegaEmbed("#min_vs_max", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+vegaEmbed("#pie_chart", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+
+vegaEmbed("#rain_vs_temp", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
